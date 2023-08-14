9ba66f0b3d24d370e860e18127c804642e878fc8
bkpark71 <bokyoungp@naver.com>
2023-08-14 14:48:07 +0900

    4일차 V1.0

---
+++ src/chapter05/ArrayClass1Ex.java
@@ -0,0 +1,9 @@
+package chapter05;
+
+public class ArrayClass1Ex {
+  public static void main(String[] args) {
+    // findArea() 가 있는  Circle 을 원소로 가지는 배열 , 배열의 크기는 5
+    // Circle의 radius 다르게 초기화(1,2,3,4,5)
+    // 5개의 원의 넓이를 출력
+  }
+}
