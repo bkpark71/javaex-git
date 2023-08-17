868f01851a05391542e4a1ab9027885363c2b9b1
bkpark71 <bokyoungp@naver.com>
2023-08-17 12:35:32 +0900

    6일차 V2.0

--- src/chapter06/TypeDownCasting.java
+++ src/chapter06/TypeDownCasting.java
@@ -7,6 +7,7 @@ public class TypeDownCasting {
 //    s.work();
     Student s = new Student();
     Person p = new Person();
+    Worker w = new Worker();
 
     System.out.println("s instanceof Student ==> " + (s instanceof Student));
     System.out.println("s instanceof Person ==> " + (s instanceof Person));
@@ -16,14 +17,20 @@ public class TypeDownCasting {
 
     downcast(p);
     downcast(s);
+    downcast(w);
+    //downcast("hello");
+    downcast(new Person());
   }
 
   static void downcast(Person p) {
-    p.whoAmI();
     if(p instanceof Student) {
       Student s = (Student) p;
       s.work();
-      s.whoAmI();
+    } else if (p instanceof Worker){
+      Worker w = (Worker) p;
+      w.work();
+    } else {
+      p.whoAmI();
     }
   }
 //  static void downcast(Student s) {
