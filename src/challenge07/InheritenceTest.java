b07902d475e2ef837ae0a2c5700c0c73ce3f581c
bkpark71 <bokyoungp@naver.com>
2023-08-18 16:44:29 +0900

    7일차 V4.0

---
+++ src/challenge07/InheritenceTest.java
@@ -0,0 +1,22 @@
+package challenge07;
+//
+//public class InheritenceTest {
+//}
+//
+//class Parent {
+//  public String name;
+//
+//  public Parent(String name){
+//    this.name = name;
+//  }
+//}
+//
+//class Child extends Parent {
+//  public int studentNo;
+//
+//  public Child(String name, int studentNo){
+//    super(name);
+//    //this.name = name;
+//    this.studentNo = studentNo;
+//  }
+//}
