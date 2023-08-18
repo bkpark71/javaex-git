9e78b7dbc3ba30ad6db93f716e151d1e20ace47e
bkpark71 <bokyoungp@naver.com>
2023-08-18 10:39:33 +0900

    7일차 V1.0

---
+++ src/challenge06/PrinterTest.java
@@ -0,0 +1,11 @@
+package challenge06;
+
+public class PrinterTest {
+  public static void main(String[] args) {
+    //Printer printer = new Printer();
+    Printer.println(10);
+    Printer.println(5.7);
+    Printer.println(true);
+    Printer.println("홍길동");
+  }
+}
